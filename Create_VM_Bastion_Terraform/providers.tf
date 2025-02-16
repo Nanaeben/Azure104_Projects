cef562b8e75d82d7d6babb429ca142d691eef1d1
Nanaeben <aboagyeeben2007@gmail.com>
2025-02-16 20:44:14 +0000

    Day03 afternoon

---
+++ Create_VM_Bastion_Terraform/providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "=3.0"
+    }
+
+  }
+}
+
+provider "azurerm" {
+     # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+    features {}
+  
+}
